dc73cb88a513d7d142f81f140954c39a63d473f0
Mitko Tschimev <mitko@dev-thought.cool>
2020-02-07 08:59:15 +0400

    feat: integrated custom domain azure
    - still struggling with connect domain with cdn

--- src/init/files/azure/angular/index.ts
+++ src/init/files/azure/angular/index.ts
@@ -2,13 +2,15 @@ import * as azure from '@pulumi/azure';
 import * as pulumi from '@pulumi/pulumi';
 import { StorageStaticWebsite } from './static-website.resource';
 import { StorageSyncResource } from './storage-sync.resource';
+import { CDNCustomDomainResource } from './cdnCustomDomain';
 
 const stackConfig = new pulumi.Config();
 const config = {
   // ===== DONT'T TOUCH THIS -> CONFIG REQUIRED BY NG-DEPLOY-UNIVERSAL ======
   projectName: stackConfig.get('projectName'),
   distPath: stackConfig.get('distPath'),
-  useCdn: stackConfig.getBoolean('useCdn')
+  useCdn: stackConfig.getBoolean('useCdn'),
+  customDomainName: stackConfig.get('customDomainName')
   // ===== END ======
 };
 const projectName = config.projectName;
@@ -38,16 +40,20 @@ const syncFiles = new StorageSyncResource('sync', {
 });
 
 let cdnEndpointResource: azure.cdn.Endpoint;
+let cdnCustomDomainResource: CDNCustomDomainResource;
 if (config.useCdn) {
   // Optionally, we can add a CDN in front of the website
-  const cdn = new azure.cdn.Profile(`pr-cdn`, {
+  const cdnProfile = new azure.cdn.Profile(`pr-cdn`, {
     resourceGroupName: resourceGroup.name,
     sku: 'Standard_Microsoft'
   });
 
   cdnEndpointResource = new azure.cdn.Endpoint(`cdn-ep`, {
+    name:
+      (config.customDomainName && config.customDomainName.replace('.', '-')) ||
+      undefined,
     resourceGroupName: resourceGroup.name,
-    profileName: cdn.name,
+    profileName: cdnProfile.name,
     originHostHeader: staticWebsiteResource.hostName,
     origins: [
       {
@@ -56,9 +62,34 @@ if (config.useCdn) {
       }
     ]
   });
+
+  if (config.customDomainName) {
+    cdnCustomDomainResource = new CDNCustomDomainResource(
+      'cdnCustomDomain',
+      {
+        resourceGroupName: resourceGroup.name,
+        // Ensure that there is a CNAME record for mycompany.com pointing to my-cdn-endpoint.azureedge.net.
+        // You would do that in your domain registrar's portal.
+        customDomainHostName: config.customDomainName,
+        profileName: cdnProfile.name,
+        endpointName: cdnEndpointResource.name,
+        /**
+         * This will enable HTTPS through Azure's one-click
+         * automated certificate deployment. The certificate is
+         * fully managed by Azure from provisioning to automatic renewal
+         * at no additional cost to you.
+         */
+        httpsEnabled: true
+      },
+      { parent: cdnEndpointResource }
+    );
+  }
 }
 
 export const staticEndpoint = staticWebsiteResource.endpoint;
 export const cdnEndpoint =
   cdnEndpointResource &&
   pulumi.interpolate`https://${cdnEndpointResource.hostName}/`;
+export const cdnCustomDomain =
+  cdnCustomDomainResource &&
+  pulumi.interpolate`https://${config.customDomainName}`;
