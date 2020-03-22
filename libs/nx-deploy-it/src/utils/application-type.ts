40349ac654ce06882298d5b6f861bebbf2575038
Mitko Tschimev <mitko@dev-thought.cool>
2020-03-22 23:46:41 +0400

    feat(nx-deploy-it): better recognation of node app
    - nestjs is recognized by ts ast now
    - express is recognized by ts ast now

--- libs/nx-deploy-it/src/utils/application-type.ts
+++ libs/nx-deploy-it/src/utils/application-type.ts
@@ -1,9 +1,14 @@
 import { TargetDefinition } from '@angular-devkit/core/src/workspace';
+import { resolve } from 'path';
+import * as ts from 'typescript';
+import { readFileSync } from 'fs-extra';
+import { hasImport } from './ats.utils';
 
 export enum ApplicationType {
   ANGULAR = 'angular',
   REACT = 'react',
-  NESTJS = 'nestjs'
+  NESTJS = 'nestjs',
+  EXPRESS = 'express'
 }
 
 function isAngular(target: TargetDefinition): boolean {
@@ -11,7 +16,34 @@ function isAngular(target: TargetDefinition): boolean {
 }
 
 function isNestJS(target: TargetDefinition): boolean {
-  return target.builder === '@nrwl/node:build';
+  if (target.builder !== '@nrwl/node:build') {
+    return false;
+  }
+  const mainPath = resolve(target.options.main.toString());
+  const mainSource = readFileSync(mainPath).toString('utf-8');
+  const main = ts.createSourceFile(
+    mainPath,
+    mainSource,
+    ts.ScriptTarget.Latest
+  );
+  return hasImport(main.statements, '@nestjs');
+}
+
+function isExpressJS(target: TargetDefinition): boolean {
+  if (target.builder !== '@nrwl/node:build') {
+    return false;
+  }
+  const mainPath = resolve(target.options.main.toString());
+  const mainSource = readFileSync(mainPath).toString('utf-8');
+  const main = ts.createSourceFile(
+    mainPath,
+    mainSource,
+    ts.ScriptTarget.Latest
+  );
+  return (
+    !hasImport(main.statements, '@nestjs') &&
+    hasImport(main.statements, 'express')
+  );
 }
 
 function isReact(target: TargetDefinition): boolean {
@@ -36,6 +68,9 @@ export function getApplicationType(target: TargetDefinition): ApplicationType {
 
   if (isNestJS(target)) {
     return ApplicationType.NESTJS;
+  }
+  if (isExpressJS(target)) {
+    return ApplicationType.EXPRESS;
   }
 
   return null;
