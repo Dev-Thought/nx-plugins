6173723f171634a861f6448bdd05930b14d6c99b
Mitko Tschimev <mitko@dev-thought.cool>
2020-03-27 09:47:25 +0400

    chore(nx-deploy-it): add test
    - add tests for azure init
    - add tests for aws init
    - add tests for gcp init

--- libs/nx-deploy-it/src/schematics/init/schematic.spec.ts
+++ libs/nx-deploy-it/src/schematics/init/schematic.spec.ts
@@ -3,24 +3,19 @@ import { SchematicTestRunner } from '@angular-devkit/schematics/testing';
 import { createEmptyWorkspace } from '@nrwl/workspace/testing';
 import { join } from 'path';
 import { NxDeployItInitSchematicSchema } from './schema';
-import { readJsonInTree } from '@nrwl/workspace';
+import { readJsonInTree, readWorkspaceJson } from '@nrwl/workspace';
 import { stdin } from 'mock-stdin';
 
 import { PROVIDER } from '../../utils/provider';
 import { createPulumiMockProjectInTree } from '../../utils-test/pulumi.mock';
 import * as childProcess from 'child_process';
 import * as fs from 'fs';
-
-// Key codes
-const keys = {
-  up: '\x1B\x5B\x41',
-  down: '\x1B\x5B\x42',
-  enter: '\x0D',
-  space: '\x20'
-};
-
-// helper function for timing
-const delay = ms => new Promise(resolve => setTimeout(resolve, ms));
+import { createApplication } from '../../utils-test/app.utils';
+import {
+  answerInitQuestionsAWS,
+  answerInitQuestionsAzure,
+  answerInitQuestionsGCP
+} from '../../utils-test/enquirer.utils';
 
 describe('init schematic', () => {
   let appTree: Tree;
@@ -48,28 +43,23 @@ describe('init schematic', () => {
       provider: 'aws'
     };
 
+    beforeEach(async () => {
+      appTree = await createApplication(
+        testRunner,
+        projectName,
+        'nest',
+        appTree
+      );
+
+      spawnSync.mockImplementation(() => {
+        createPulumiMockProjectInTree(appTree, PROVIDER.AWS, projectName);
+        return {} as any;
+      });
+      unlinkSync.mockImplementation();
+    });
+
     it('should add dependencies to package.json', async () => {
-      appTree = await testRunner
-        .runExternalSchematicAsync(
-          '@nrwl/nest',
-          'application',
-          {
-            name: projectName
-          },
-          appTree
-        )
-        .toPromise();
-
-      const initQuestions = async () => {
-        // aws region
-        io.send(keys.enter);
-        await delay(10);
-
-        // aws profile
-        io.send('my-aws-profile');
-        io.send(keys.enter);
-      };
-      setTimeout(() => initQuestions().then(), 5);
+      answerInitQuestionsAWS(io, null, null);
 
       spawnSync.mockImplementation(() => {
         createPulumiMockProjectInTree(appTree, PROVIDER.AWS, projectName);
@@ -83,6 +73,143 @@ describe('init schematic', () => {
       const packageJSON = readJsonInTree(tree, 'package.json');
       expect(packageJSON.dependencies['@pulumi/pulumi']).toBeDefined();
       expect(packageJSON.dependencies['@pulumi/aws']).toBeDefined();
+      expect(packageJSON.dependencies['@pulumi/awsx']).toBeDefined();
+    });
+
+    it('should extend the project configuration options with aws profile', async () => {
+      answerInitQuestionsAWS(io, 'eu-central-1', 'my-aws-profile');
+
+      const tree = await testRunner
+        .runSchematicAsync('init', options, appTree)
+        .toPromise();
+
+      const workspaceJson = readJsonInTree(tree, 'workspace.json');
+      expect(
+        workspaceJson.projects[projectName].architect.deploy
+      ).toMatchSnapshot();
+      expect(
+        workspaceJson.projects[projectName].architect.destroy
+      ).toMatchSnapshot();
+    });
+
+    it('should extend the project default configuration options', async () => {
+      answerInitQuestionsAWS(io, 'eu-central-1');
+
+      const tree = await testRunner
+        .runSchematicAsync('init', options, appTree)
+        .toPromise();
+
+      const workspaceJson = readJsonInTree(tree, 'workspace.json');
+      expect(
+        workspaceJson.projects[projectName].architect.deploy
+      ).toMatchSnapshot('Deploy Action');
+      expect(
+        workspaceJson.projects[projectName].architect.destroy
+      ).toMatchSnapshot('Destroy Action');
+    });
+  });
+
+  describe('azure provider', () => {
+    const options: NxDeployItInitSchematicSchema = {
+      project: projectName,
+      provider: 'azure'
+    };
+
+    beforeEach(async () => {
+      appTree = await createApplication(
+        testRunner,
+        projectName,
+        'nest',
+        appTree
+      );
+
+      spawnSync.mockImplementation(() => {
+        createPulumiMockProjectInTree(appTree, PROVIDER.AZURE, projectName);
+        return {} as any;
+      });
+      unlinkSync.mockImplementation();
+    });
+
+    it('should add dependencies to package.json', async () => {
+      answerInitQuestionsAzure(io, null);
+
+      const tree = await testRunner
+        .runSchematicAsync('init', options, appTree)
+        .toPromise();
+
+      const packageJSON = readJsonInTree(tree, 'package.json');
+      expect(packageJSON.dependencies['@pulumi/pulumi']).toBeDefined();
+      expect(packageJSON.dependencies['@pulumi/azure']).toBeDefined();
+    });
+
+    it('should extend the project default configuration options', async () => {
+      answerInitQuestionsAzure(io, 'eastasia');
+
+      const tree = await testRunner
+        .runSchematicAsync('init', options, appTree)
+        .toPromise();
+
+      const workspaceJson = readJsonInTree(tree, 'workspace.json');
+      expect(
+        workspaceJson.projects[projectName].architect.deploy
+      ).toMatchSnapshot('Deploy Action');
+      expect(
+        workspaceJson.projects[projectName].architect.destroy
+      ).toMatchSnapshot('Destroy Action');
+    });
+  });
+
+  describe('google cloud platform provider', () => {
+    const options: NxDeployItInitSchematicSchema = {
+      project: projectName,
+      provider: 'gcp'
+    };
+
+    beforeEach(async () => {
+      appTree = await createApplication(
+        testRunner,
+        projectName,
+        'nest',
+        appTree
+      );
+
+      spawnSync.mockImplementation(() => {
+        createPulumiMockProjectInTree(
+          appTree,
+          PROVIDER.GOOGLE_CLOUD_PLATFORM,
+          projectName
+        );
+        return {} as any;
+      });
+      unlinkSync.mockImplementation();
+    });
+
+    it('should add dependencies to package.json', async () => {
+      answerInitQuestionsGCP(io, 'my-google-project-id', 'europe-west1');
+
+      const tree = await testRunner
+        .runSchematicAsync('init', options, appTree)
+        .toPromise();
+
+      const packageJSON = readJsonInTree(tree, 'package.json');
+      expect(packageJSON.dependencies['@pulumi/pulumi']).toBeDefined();
+      expect(packageJSON.dependencies['@pulumi/gcp']).toBeDefined();
+    });
+
+    it('should extend the project default configuration options', async () => {
+      answerInitQuestionsGCP(io, 'my-google-project-id', 'europe-west1');
+
+      const tree = await testRunner
+        .runSchematicAsync('init', options, appTree)
+        .toPromise();
+
+      const workspaceJson = readJsonInTree(tree, 'workspace.json');
+      expect(
+        workspaceJson.projects[projectName].architect.deploy
+      ).toMatchSnapshot('Deploy Action');
+      expect(
+        workspaceJson.projects[projectName].architect.destroy
+      ).toMatchSnapshot('Destroy Action');
     });
   });
 });
