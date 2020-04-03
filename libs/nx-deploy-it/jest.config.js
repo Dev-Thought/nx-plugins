537bcdbcef664d1997c8a03bda3324e7203bfb43
Mitko Tschimev <mitko@dev-thought.cool>
2020-04-03 08:10:41 +0400

    feat(nx-deploy-it): add coverage
    - activate collect coverage per default
    - add global thresholds for nx-deploy-it

--- libs/nx-deploy-it/jest.config.js
+++ libs/nx-deploy-it/jest.config.js
@@ -5,5 +5,13 @@ module.exports = {
     '^.+\\.[tj]sx?$': 'ts-jest'
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'html'],
-  coverageDirectory: '../../coverage/libs/nx-deploy-it'
+  coverageDirectory: '../../coverage/libs/nx-deploy-it',
+  coverageThreshold: {
+    global: {
+      branches: 59,
+      functions: 85,
+      lines: 83,
+      statements: 83
+    }
+  }
 };
